dda91b3e335eb5f54b1fdc2f16fb75dc04b52087
Arsh <arshagarwal.18@gmail.com>
2020-10-04 15:14:22 +0530

    changing G init

--- solver.py
+++ solver.py
@@ -96,7 +96,7 @@ class Solver(object):
     def build_model(self):
         """Create a generator and a discriminator."""
 
-        self.G = Generator(img_size=self.img_size[-1], style_dim=self.args.style_dim)
+        self.G = Generator(img_size=self.img_size[0], style_dim=self.args.style_dim)
         self.D = Discriminator(img_size=self.img_size, num_domains=self.c_dim)
         self.M = MappingNetwork(self.args.latent_dim, self.args.style_dim, self.c_dim)
 
